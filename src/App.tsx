53e81413ba9b0d5516f5c9a0974060da5043d849
Michael Eselgroth <michael.e@canva.com>
2020-12-14 10:35:36 +1100

    Start simple table

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,28 @@ import './App.css';
 import { observer } from "mobx-react"
 import Store from './Store';
 
+
+
 const App = observer(({ store }: { store: Store }) => 
   <div className="App">
-    <h1>JS Starcraft Bot</h1>
+    <table>
+      <thead>
+      <tr>
+          <td>A</td>
+          <td>A</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>A</td>
+          <td>A</td>
+        </tr>
+        <tr>
+          <td>A</td>
+          <td>A</td>
+        </tr>
+      </tbody>
+    </table>
     {store.armyCount}
   </div>
 );
