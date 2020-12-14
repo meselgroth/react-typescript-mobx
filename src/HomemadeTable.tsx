a6a34657e2e74f4e9171dae80c807e371b17370e
Michael Eselgroth <michael.e@canva.com>
2020-12-14 16:44:36 +1100

    Add updates to ensure observable is working. Fix cell observable.

--- src/HomemadeTable.tsx
+++ src/HomemadeTable.tsx
@@ -4,11 +4,11 @@ import { observer } from "mobx-react";
 import Store, { Entity } from './Store';
 
 
-const Cells = ({ app }: { app: Entity }) =>
+const Cells = observer(({ app }: { app: Entity }) =>
   <>
     <td>{app.id}</td>
     <td>{app.name}</td>
-  </>
+  </>);
 
 const Row = ({ children }: { children: ReactNode }) =>
   <tr>
@@ -34,10 +34,13 @@ const Table = ({ children }: TableProps) =>
 
 const HomemadeTable = observer(({ store }: { store: Store }) => {
   const rows = store.apps.map(app =>
-    <Row>
+    <Row key={app.id}>
       <Cells app={app} />
     </Row>
   );
+
+
+
   return <div className="App">
     <div>{store.appsCount}</div>
     <Table>
--- src/Store.tsx
+++ src/Store.tsx
@@ -1,30 +1,47 @@
 import { computed, makeObservable, observable } from "mobx";
 
 export default class Store {
-    apps: Array<Entity> = [];
+  apps: Array<Entity> = [];
 
-    constructor() {
-        makeObservable(this, {
-            apps: observable,
-            appsCount: computed
-        })
-        this.apps = [{
-            id: '1',
-            name: 'Abc'
-        },
-        {
-            id: '2',
-            name: 'Def'
-        }
-        ];
-    }
+  constructor() {
+    makeObservable(this, {
+      apps: observable,
+      appsCount: computed
+    })
+    this.apps = [{
+      id: '1',
+      name: 'Abc'
+    },
+    {
+      id: '2',
+      name: 'Def'
+    }];
 
-    get appsCount(){
-        return this.apps.length;
-    }
+    setInterval(() => {
+      this.changeName('1', 'abc' + Math.round(Math.random() * 10));
+    }, 1000);
+  
+    let idCount = 3;
+    setInterval(() => {
+      this.addApp({ id: idCount.toString(), name: (Math.round(Math.random() * 10)).toString() });
+      idCount++;
+    }, 10000);
+  }
+
+  changeName(id: string, newName: string) {
+    this.apps.filter(app => app.id === id)[0].name = newName;
+  }
+
+  addApp(app: Entity) {
+    this.apps.push(app);
+  }
+
+  get appsCount() {
+    return this.apps.length;
+  }
 }
 
 export interface Entity {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 }
