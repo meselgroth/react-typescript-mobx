210690ab997d964a0f17c8dca1d4b1c76eec03a6
Michael Eselgroth <michael.e@canva.com>
2020-12-29 10:21:38 +1100

    Correctly type getComparator, remove any type

--- src/EnhancedTable/stableSort.ts
+++ src/EnhancedTable/stableSort.ts
@@ -1,26 +1,21 @@
 export type Order = 'asc' | 'desc';
 
 export function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-    if (b[orderBy] < a[orderBy]) {
-      return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-      return 1;
-    }
-    return 0;
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
   }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
 
-export function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key): (a: {
-    [key in Key]: number | string;
-  }, b: {
-    [key in Key]: number | string;
-  }) => number {
+export function getComparator<T>(order: Order, orderBy: keyof T):
+  (a: T, b: T) => number {
   return order === 'desc'
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
-}  
+}
 
 export function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
   const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
